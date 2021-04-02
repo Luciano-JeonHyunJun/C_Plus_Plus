64f7520b23e1c88a30fda739ffa444dc95c98e5e
전현준Jeonhyenjun <jhj091542@gmail.com>
2021-04-03 02:06:17 +0900

    Add files via upload

---
+++ Hello_World.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    cout << "Hello C++" << endl;
+    return 0;
+}
+
+
+/*
+#include <iostream> : 전처리 지시자 라고 말합니다.
+ - c++에서 함수를 사용하려면 함수의 원형을 사용해야합니다.
+
+c++에서 ;(세미콜론) 을 종결자로 활용합니다.
+c++에서는 main의 이름을 가진 함수가 있어야한다.
+
+cout는 cout 다음에 나오는 문장을 터미널에 출력시킵니다.
+endl은 줄바꿈 역활을 합니다.
+만약 endl이 없다면 줄바꿈을 하지 않은채로 출력이 됩니다.
+<< 두개는 데이터에 방향을 나타냅니다. 
+using namespace std;(iostream의 함수) 이게 없다면 cout 앞에
+"std::" 을 붙여줘야합니다. 
+
+주석은 // 과 현재 주석입니다.입니다.
+ */
+
