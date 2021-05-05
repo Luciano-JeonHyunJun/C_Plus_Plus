785a913008c10600e7c5da000289ceeadc623655
전현준Jeonhyenjun <jhj091542@gmail.com>
2021-05-05 23:05:18 +0900

    Add files via upload

---
+++ CPP/if_else.cpp
@@ -0,0 +1,20 @@
+// if & else
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int x;
+    x = 100;
+
+    if (x == 100) 
+        cout << "x is 100";
+    else if (x < 0)
+        cout << "x is not 100";
+    else 
+        cout << "x is 0";
+
+    return 0;
+}
+
