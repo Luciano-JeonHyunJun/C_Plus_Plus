b45fffb5ef1e8faa833b922da3730b6d4464eecf
전현준Jeonhyenjun <jhj091542@gmail.com>
2021-05-05 23:06:18 +0900

    Add files via upload

---
+++ CPP/input_output.cpp
@@ -0,0 +1,20 @@
+/**
+cin : 입력 스트림
+cout : 출력 스트림
+cerr : 오류 출력 스트림
+clog : 로깅 출력 스트림
+**/
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int i;
+    cout << "안녕 몇살이니?";
+    cin >> i;
+    cout << "아 나보다 어리구나";
+    cout << "나는 17살이야";
+
+    return 0;
+}
