a93532de956e1ae71e7bd20c3389f0381f029399
전현준Jeonhyenjun <jhj091542@gmail.com>
2021-05-23 00:54:17 +0900

    Update function.cpp

--- CPP/function.cpp
+++ CPP/function.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int square()
+int square(int x)
 {
 
-    return x * x
+
+    return x * x;
+}
+
+
+
+int main()
+{
+    cout << square(3) << '\n';
+
+
+
+
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int square(int x)
+{
+
+
+    return x * x;
+}
+
+
+
+int main()
+{
+    cout << square(3) << '\n';
+
+
+
+
+    return 0;
 }
